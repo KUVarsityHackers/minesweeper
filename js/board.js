402561ea34e33d4b34286e1b140428dfe36a6adf
Nathan Nichols <n545n545@ku.edu>
2019-03-05 17:18:46 -0600

    constructed the board

--- js/board.js
+++ js/board.js
@@ -1,3 +1,5 @@
+var dt = require("./boardspace.js");
+
 class Board {
   constructor(xsize, ysize, nummine, flags) {
     this.numRows = xsize;
@@ -5,6 +7,13 @@ class Board {
     this.numMines = nummine;
     this.numFlags = flags;
     this.numMinesFlagged = 0;
+    this.m_board = new Array(xsize);
+    for (let x = 0; x < xsize; x++) {
+      this.m_board[x] = new Array(ysize);
+      for (let y = 0; y < ysize; y++) {
+        this.m_board[x][y] = new BoardSpace();
+      }
+    }
   }
   takeStep() {}
   firstStep() {}
