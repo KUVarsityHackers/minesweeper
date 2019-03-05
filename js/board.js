afadbae4a685e852be6e12ed3c1e9d9c7b66b22d
Michael Svoren <michael.svoren@gmail.com>
2019-03-05 17:18:59 -0600

    added two methods

--- js/board.js
+++ js/board.js
@@ -9,4 +9,60 @@ class Board {
   takeStep() {}
   firstStep() {}
   takeStep() {}
+
+  toggleFlagSpace(row, col)
+    {
+      //see if the space is already flagged
+      if (m_board[row][col].isFlagged)
+      {
+        m_board[row][col].isFlagged = false;
+
+        //update number of mines correctly marked if this is a mine
+        if (m_board[row][col].isMine)
+        {
+          this.numMinesFlagged--;
+        }
+
+      }
+      //else if the space is not flagged, then remove
+      else if (!m_board[row][col].isFlagged && this.numFlags>0)
+      {
+        m_board[row][col].isFlagged = true;
+        this.numFlags--;
+
+        //update number of mines correctly marked if this is a mine
+        if (m_board[row][col].isMine)
+        {
+          this.numMinesFlagged++;
+        }
+      }
+      //throw an exception if no flags remain
+      else
+      {
+        throw "Out of flags";
+      }
+    }
+
+
+  selectSpace(row, col)
+  {
+    //if mine
+    if (m_board[row][col].isMine)
+    {
+      if (m_board[row][col].isFlagged)
+      {
+        return false;
+      }
+      if (m_board[row][col].isMine)
+      {
+        return true;
+      }
+    }
+    else
+    {
+      recUnhide(row, col);
+      return false;
+    }
+  }
 }
+
