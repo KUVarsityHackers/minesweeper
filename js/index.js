1185f95d96811208faa74c304eb76d3e40041615
Nathan Nichols <n545n545@ku.edu>
2019-03-18 21:21:23 -0500

    the timer reaching 0 now ends the game

--- js/index.js
+++ js/index.js
@@ -6,12 +6,11 @@ Powerups:
 
 */
 
-
 import { Board } from "./board.js";
 
 let takenFirstStep = false;
 let gameEnded = false;
-const fiveMinutes = 60 * 5;
+const fiveMinutes = 5 * 60;
 
 window.startGame = function startGame() {
   const boardHeight = Number(document.getElementById("boardHeight").value);
@@ -41,7 +40,7 @@ window.startGame = function startGame() {
 
     const board = new Board(boardHeight, boardWidth, numOfMines, numOfMines);
     drawBoard(board);
-    
+
     document.getElementById("slot").style.display = "block";
     document.getElementById("resetButton").style.display = "block";
     document.getElementById("cheatMode").style.display = "block";
@@ -51,7 +50,6 @@ window.startGame = function startGame() {
 function drawBoard(board) {
   show(board);
   $(".square").on("click", function(e) {
-
     let xPos = Number($(this).attr("data-x-coordinate"));
     let yPos = Number($(this).attr("data-y-coordinate"));
     console.log("run:" + yPos, xPos);
@@ -62,9 +60,9 @@ function drawBoard(board) {
       gameEnded = board.takeStep(yPos, xPos);
     }
     if (gameEnded) {
-      unhideMines(board); 
-      board.numSpacesLeft? endScreen("lose") : endScreen("win");
-    } 
+      unhideMines(board);
+      board.numSpacesLeft ? endScreen("lose") : endScreen("win");
+    }
     drawBoard(board);
   });
 
@@ -72,12 +70,11 @@ function drawBoard(board) {
     const elementClicked = $(this);
     const xPos = elementClicked.attr("data-x-coordinate");
     const yPos = elementClicked.attr("data-y-coordinate");
-    if (e.which == 3 && gameEnded == false) {   
+    if (e.which == 3 && gameEnded == false) {
       board.toggleFlagSpace(yPos, xPos);
-      drawBoard(board)
+      drawBoard(board);
     }
   });
-  
 }
 
 // /**
@@ -157,7 +154,7 @@ function endScreen(condition) {
   if (condition == "win") {
     alert("You Won!");
   } else {
-    alert("Game Over. Try again?");
+    alert("Game Over. Try again");
   }
   gameEnded = true;
 }
@@ -166,9 +163,9 @@ function endScreen(condition) {
 //Taken from https://stackoverflow.com/questions/20618355/the-simplest-possible-javascript-countdown-timer
 let timer = 0;
 function startTimer(duration, display) {
-  (timer = duration);
+  timer = duration;
   let minutes, seconds;
-  setInterval(function() {
+  var countdown = setInterval(function() {
     minutes = parseInt(timer / 60, 10);
     seconds = parseInt(timer % 60, 10);
 
@@ -178,7 +175,8 @@ function startTimer(duration, display) {
     display.textContent = minutes + ":" + seconds;
 
     if (--timer < 0) {
-      timer = duration;
+      endScreen("lose");
+      clearInterval(countdown);
     }
   }, 1000);
 }
@@ -192,8 +190,6 @@ function addTime() {
   let display = document.querySelector("#time");
   startTimer(timer + 30, display);
 }
-
-removeTime();
 
 function removeTime() {
   console.log("func run");
@@ -217,20 +213,18 @@ function show(board) {
       if (!square.isHidden) {
         if (square.isMine) {
           squareElement.addClass("exploded-square");
-        }
-        else {
+        } else {
           squareElement.addClass("empty-square");
-          if(square.numMines != 0) {
+          if (square.numMines != 0) {
             squareElement.html(square.numMines);
           }
         }
-      }
-      else {
+      } else {
         squareElement.addClass("square");
         if (square.isFlagged) {
-        squareElement.addClass("flagged-square");
+          squareElement.addClass("flagged-square");
+        }
       }
-    }
       squareElement.attr("data-x-coordinate", col);
       squareElement.attr("data-y-coordinate", row);
       rowElement.append(squareElement);
@@ -256,33 +250,30 @@ function unhideMines(board, hidden = true) {
 let slot = document.getElementById("slotButton");
 
 let count = 8;
-let one = Math.floor((Math.random()*9%4)+1);
-let two = Math.floor((Math.random()*9%4)+1);
-let three = Math.floor((Math.random()*9%4)+1);
+let one = Math.floor(((Math.random() * 9) % 4) + 1);
+let two = Math.floor(((Math.random() * 9) % 4) + 1);
+let three = Math.floor(((Math.random() * 9) % 4) + 1);
 
-slot.onclick  = function(){
+slot.onclick = function() {
+  if (count > 0) {
+    one = Math.floor(((Math.random() * 9) % 3) + 1);
+    two = Math.floor(((Math.random() * 9) % 3) + 1);
+    three = Math.floor(((Math.random() * 9) % 3) + 1);
+    count--;
 
-  if (count > 0)    
-    {
-        one = Math.floor((Math.random()*9%3)+1);
-        two = Math.floor((Math.random()*9%3)+1);
-        three = Math.floor((Math.random()*9%3)+1);
-        count--;
+    slot.value = "Spins: " + count;
 
-        slot.value = "Spins: " + count;
+    document.getElementById("num1").innerHTML = one;
+    document.getElementById("num2").innerHTML = two;
+    document.getElementById("num3").innerHTML = three;
 
-        document.getElementById("num1").innerHTML = one;
-        document.getElementById("num2").innerHTML = two;
-        document.getElementById("num3").innerHTML = three;
-
-        if(count == 0){
-          slot.style.display = "none";
-        }
+    if (count == 0) {
+      slot.style.display = "none";
     }
-    //win condition here
-    if (one == two && one == three && one != null)    
-    {
+  }
+  //win condition here
+  if (one == two && one == three && one != null) {
     console.log("winner");
     //call powerup function
-    }
-}
+  }
+};
