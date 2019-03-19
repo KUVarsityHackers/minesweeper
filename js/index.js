2f53d00e096788e1235b1ec8067251b511487799
TiernonRR <tiernon.riesenmy@gmail.com>
2019-03-18 21:00:57 -0500

    testing on powerups

--- js/index.js
+++ js/index.js
@@ -45,6 +45,40 @@ window.startGame = function startGame() {
     document.getElementById("slot").style.display = "block";
     document.getElementById("resetButton").style.display = "block";
     document.getElementById("cheatMode").style.display = "block";
+
+    let slot = document.getElementById("slotButton");
+
+    let count = 8;
+    let one = Math.floor((Math.random()*9%4)+1);
+    let two = Math.floor((Math.random()*9%4)+1);
+    let three = Math.floor((Math.random()*9%4)+1);
+      
+    slot.onclick  = function(){
+
+    if (count > 0)    
+      {
+          one = Math.floor((Math.random()*9%3)+1);
+          two = Math.floor((Math.random()*9%3)+1);
+          three = Math.floor((Math.random()*9%3)+1);
+          count--;
+
+          slot.value = "Spins: " + count;
+
+          document.getElementById("num1").innerHTML = one;
+          document.getElementById("num2").innerHTML = two;
+          document.getElementById("num3").innerHTML = three;
+
+          if(count == 0){
+            slot.style.display = "none";
+          }
+          
+      }
+      //win condition here
+      if (one == two && one == three && one != null)    
+      {
+
+      }
+}
   }
 };
 
@@ -252,36 +286,4 @@ function unhideMines(board, hidden = true) {
     https://codereview.stackexchange.com/questions/51532/html-js-slot-machine-simulator    
 */
 
-let slot = document.getElementById("slotButton");
 
-let count = 8;
-let one = Math.floor((Math.random()*9%4)+1);
-let two = Math.floor((Math.random()*9%4)+1);
-let three = Math.floor((Math.random()*9%4)+1);
-
-slot.onclick  = function(){
-
-  if (count > 0)    
-    {
-        one = Math.floor((Math.random()*9%3)+1);
-        two = Math.floor((Math.random()*9%3)+1);
-        three = Math.floor((Math.random()*9%3)+1);
-        count--;
-
-        slot.value = "Spins: " + count;
-
-        document.getElementById("num1").innerHTML = one;
-        document.getElementById("num2").innerHTML = two;
-        document.getElementById("num3").innerHTML = three;
-
-        if(count == 0){
-          slot.style.display = "none";
-        }
-    }
-    //win condition here
-    if (one == two && one == three && one != null)    
-    {
-    console.log("winner");
-    //call powerup function
-    }
-}
